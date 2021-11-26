ab8616ceec6ce86ede8c89ff3601649533d85792
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-11-26 13:11:26 -0500

    Initialize BigNumber component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import BigNumber from "./components/BigNumber";
 
 function App() {
   return (
     <div className="App">
       <p>My website</p>
+      <BigNumber />
     </div>
   );
 } 
---
+++ src/components/BigNumber.jsx
@@ -0,0 +1,11 @@
+import './BigNumber.css';
+
+function BigNumber() {
+  return (
+    <div className="BigNumber">
+      <p>Big Number widget</p>
+    </div>
+  );
+} 
+
+export default BigNumber;
